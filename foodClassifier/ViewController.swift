60fa91489221b951c3e8b4b8b3bed61ac3eb1737
Sero7777 <max_wa@gmx.de>
2019-11-20 14:52:46 +0100

    Initial Commit

---
+++ foodClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodClassifier
+//
+//  Created by Max Wagner on 20.11.19.
+//  Copyright © 2019 Max Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
